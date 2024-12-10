09495eb2514cdc029a7c92097c95e989cdb23cbf
Roman Chernych <LRCSlonik@mail.ru>
2024-12-10 03:08:42 +0300

    na proverku

---
+++ src/main/java/app/model/AnimalsCage.java
@@ -0,0 +1,31 @@
+package app.model;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AnimalsCage {
+
+    @Autowired
+    @Qualifier("dog")
+    private Animal animal;
+
+    @Autowired
+    @Qualifier("timer")
+    private Timer timer;
+
+    @Bean
+    public Timer getTimer() {
+        return this.timer;
+    }
+
+    public void whatAnimalSay() {
+        System.out.println("Say:");
+        System.out.println(animal.toString());
+        System.out.println("At:");
+        System.out.println(timer.getTime());
+        System.out.println("________________________");
+    }
+}
